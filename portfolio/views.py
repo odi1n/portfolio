66d10ba6fcc9a637dc4b445737a65388b3f01228
odi1n <sashf1999@mail.ru>
2021-10-08 17:01:35 +0300

    Добавил views для создание .pdf файла Настроил urls

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from io import BytesIO
 
-# Create your views here.
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
+
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+
+
+from django.http import HttpResponse
+from django.views.generic import View
+
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('mytemplates.html')
+        context = {
+            "invoice_id": 123,
+            "customer_name": "John Cooper",
+            "amount": 1399.99,
+            "today": "Today",
+        }
+        html = template.render(context)
+        pdf = render_to_pdf('mytemplates.html', context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" % ("12341231")
+            content = "inline; filename='%s'" % (filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" % (filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found")
--- portfolio_generate/settings.py
+++ portfolio_generate/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Set .env setting
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # reading .env file
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -35,7 +34,6 @@ SECRET_KEY = env.str('SECRET_KEY', default=get_random_string(50, 'abcdefghijklmn
 DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -66,7 +64,7 @@ ROOT_URLCONF = 'portfolio_generate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'portfolio/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -82,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portfolio_generate.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -92,7 +89,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -112,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -125,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
