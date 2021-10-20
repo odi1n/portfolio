29f0ee3887c97094b3dcd7cd597525c1dcb2222f
odi1n <sashf1999@mail.ru>
2021-10-20 20:57:16 +0300

    Поправил настройки .env, и вынес на development, production

---
+++ portfolio_generate/settings/production.py
@@ -0,0 +1,22 @@
+from .base import *
+
+# Secret Key
+SECRET_KEY = env.str('SECRET_KEY')
+
+# Debug
+DEBUG = env.bool('DEBUG')
+
+# Allowed
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+# Database Setting
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str("POSTGRES_DB"),
+        'USER': env.str("POSTGRES_USER"),
+        'PASSWORD': env.str("POSTGRES_PASSWORD"),
+        'HOST': env.str("POSTGRES_HOST"),
+        'POST': env.str("POSTGRES_POST"),
+    }
+}
