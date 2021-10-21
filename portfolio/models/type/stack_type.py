e82d889c15d328e8d103680d25d3fc236ae01477
odi1n <sashf1999@mail.ru>
2021-10-21 15:31:34 +0300

    Стэк сделал через ,

--- portfolio/models/type/stack_type.py
+++ portfolio/models/type/stack_type.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class StackType(models.IntegerChoices):
-    FULLSTACK = 0, _('Fullsack')
-    BACKEND = 1, _('Backend')
-    FRONTEND = 2, _('Frontend')
+    FULLSTACK = 0, _('Fullstack')
+    BACKEND = 1, _('Back-End')
+    FRONTEND = 2, _('Front-End')
