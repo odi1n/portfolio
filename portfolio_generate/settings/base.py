fd7b60a139f954fd666d3d481f94975e398c36b9
odi1n <sashf1999@mail.ru>
2021-10-21 15:33:01 +0300

    Разделил некоторые поля base

--- portfolio_generate/settings/base.py
+++ portfolio_generate/settings/base.py
@@ -122,9 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-# STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media
 MEDIA_URL = '/media/'
