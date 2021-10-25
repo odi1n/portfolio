fb12629a437b446496349aa87c9733d4d2d2a4b4
odi1n <sashf1999@mail.ru>
2021-10-25 10:47:25 +0300

    Поправил вывод "Опыт работы" в шаблоне

--- portfolio/templatetags/my_tags.py
+++ portfolio/templatetags/my_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils.timezone import now
+import locale
+locale.setlocale(locale.LC_ALL,'')
 
 register = template.Library()
 
@@ -11,3 +14,11 @@ def dict_join(qs):
         if index != qs.count() - 1:
             values += ' / '
     return values
+
+@register.filter()
+def work_place_tags(qs):
+    if qs.date_end is  None:
+        value = f'По текущий момент'
+    elif qs.date_end is not None:
+        value = qs.date_end.strftime("%d %B %Y")
+    return f'{qs.text} - {qs.whom}. {qs.date_start.strftime("%d %B %Y")} - {value}'
