86833ea622209c6bcbc01643670aa03da2aa596c
odi1n <sashf1999@mail.ru>
2021-10-25 10:48:53 +0300

    Дополнил модель "Место работы"

--- users/models/work_places.py
+++ users/models/work_places.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.timezone import now
+
 from .custom_user import CustomUser
 
 
@@ -7,8 +9,19 @@ class WorkPlace(models.Model):
                              verbose_name="Пользователь",
                              on_delete=models.CASCADE)
     text = models.CharField(verbose_name="Место работы",
-                            help_text="Например: Rocket Science - Разработчик. Март 2021 - По настроящее время (8месяцев)",
+                            help_text="Название компании. Например: Rocket Science",
                             max_length=255)
+    whom = models.CharField(verbose_name="Кем работаете",
+                            help_text="Например: Разработчик, Архитектор, Программист, ....",
+                            max_length=255)
+    date_start = models.DateField(verbose_name="Начало работы",
+                                  default=now,
+                                  editable=True)
+    date_end = models.DateField(verbose_name="Конец работы",
+                                help_text="Оставить поле пустым, если сейчас работаете",
+                                editable=True,
+                                null=True,
+                                blank=True)
 
     class Meta:
         verbose_name = "Место работы"
