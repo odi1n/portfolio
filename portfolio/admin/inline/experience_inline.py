3d6b260dc8ffeb730e966ac9098a596170e2e488
odi1n <sashf1999@mail.ru>
2021-11-03 12:16:49 +0300

    Добавил inline для портфолио

---
+++ portfolio/admin/inline/experience_inline.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from ...models import Experience
+
+
+class ExperienceStackedInline(admin.StackedInline):
+    model = Experience
+    classes = ['collapse']
+    extra = 1
