2ebc6c0f275e22c2cede1ae52e2f99342a5d40c3
odi1n <sashf1999@mail.ru>
2023-05-16 03:02:16 +0300

    Поправил ошибку генерации шаблона

--- src/portfolio/service/generate_pdf.py
+++ src/portfolio/service/generate_pdf.py
@@ -1,24 +1,22 @@
-import tempfile
-
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
 from weasyprint import HTML
 
 
-def generate(templates, context, data: str):
+def generate(
+    templates,
+    context,
+    data: str,
+):
     # Обработка шаблона
-    html_string = render_to_string("mytemplates.html", context)
+    html_string = render_to_string(templates, context)
     html = HTML(string=html_string)
-    result = html.write_pdf()
 
     # Создание http ответа
     response = HttpResponse(content_type="application/pdf;")
     response["Content-Disposition"] = f"inline; filename={data}.pdf"
     response["Content-Transfer-Encoding"] = "binary"
-    with tempfile.NamedTemporaryFile(delete=True) as output:
-        output.write(result)
-        output.flush()
-        output = open(output.name, "rb")
-        response.write(output.read())
-    return response
+
+    result = html.write_pdf(response)
+    return result
