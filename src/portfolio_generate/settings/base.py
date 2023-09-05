8646b25288762406d694a8e4b1b6db14c3705533
odi1n <sashf1999@mail.ru>
2023-09-06 01:31:23 +0300

    update(poetry): django update version - 4.2.5

--- src/portfolio_generate/settings/base.py
+++ src/portfolio_generate/settings/base.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR.parent, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR.parent, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, "static")]
 
 # Media
 MEDIA_URL = "/media/"
